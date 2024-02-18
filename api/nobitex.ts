5efc4afc9cf61f75b6c223cb006b07f153478f6b
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-02-18 20:30:25 +0330

    Modify Api

--- api/nobitex.ts
+++ api/nobitex.ts
@@ -1,6 +1,7 @@
-import { nobitexSymbols, nobitexSymbolsTypes } from "../variables/nobitex";
+import {  nobitexSymbolsTypes } from "../variables/nobitex";
 
 export const NobitexEndPoint = Object.freeze ({
     ORDERBOOK  : "v2/orderbook",
-    ORDERBOOK_SYMBOL :(symbol:nobitexSymbolsTypes)=>`v2/orderbook/${symbol}`
+    ORDERBOOK_SYMBOL :(symbol:nobitexSymbolsTypes)=>`v2/orderbook/${symbol}`,
+    OHLC:'/market/udf/history',
 })
