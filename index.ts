9202f4d2535df9327719eec489cf0e752e5d90b8
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-02-18 20:30:40 +0330

    Add Tracking Symbols

--- index.ts
+++ index.ts
@@ -1,13 +1,34 @@
-import { NobitexEndPoint} from "./api/nobitex"
-import { nobitexApi} from "./config/api"
+import { AmdDependency } from "typescript"
+import { NobitexEndPoint } from "./api/nobitex"
+import { nobitexApi } from "./config/api"
+import { nobitexSymbolsRial, nobitexSymbolsUSDT } from "./variables/nobitex"
 
 setInterval(async () => {
 
     console.log('Tracking...', (new Date()).toLocaleString())
 
-    const response = await nobitexApi.get(NobitexEndPoint.ORDERBOOK_SYMBOL('BTCUSDT'))
+    //Calculate Change Percent 
 
-    console.log(response.data)
+    const responses = await Promise.all(nobitexSymbolsUSDT.slice(0,2).map(async (symbol) => ({
+        data: (await nobitexApi.get(NobitexEndPoint.OHLC, {
+            params:
+            {
+                symbol:'BTCIRT',
+                resolution: '1',
+                to: Math.floor(Date.now() / 1000),
+                from: Math.floor((Date.now() - (1000 * 60 * 60)) / 1000)
+            }
+
+        })).data,
+        symbol
+    }
+
+    ))
+
+    )
+
+    console.log(responses)
+
 
 }, Number(process.env.NODE_TRACK_TIME_INTERVAL) * 1000 * 60)
 
