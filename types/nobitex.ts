f38d5c9498f2202aefae5b0493b57c01d2807dbf
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-02-20 20:08:19 +0330

    Add Response Type

--- types/nobitex.ts
+++ types/nobitex.ts
@@ -1,6 +1,17 @@
-interface OrderBookResponseType {
-    status: 'ok' | string,
+export interface OrderBookResponseType {
+    status: NobitexStatusType,
     lastUpdate: number,
     bids: Array<[string, string]>,
     asks: Array<[string, string]>
+}
+
+export type NobitexStatusType = 'ok' | 'no_data' | 'error'
+
+export interface OHLCResponseType {
+    s: NobitexStatusType,
+    t:Array<number>,
+    c:Array<number>,
+    o:Array<number>,
+    h:Array<number>,
+    l:Array<number>,    
 }
